366fed6291682412da01979c14ca5aefff953ec4
Gitsifu <2587107273.@qq.com>
2017-10-31 23:50:25 +0800

    添加商品DAO

---
+++ src/main/java/io/github/sifu/springbootsell/repository/ProductInfoRepository.java
@@ -0,0 +1,23 @@
+package io.github.sifu.springbootsell.repository;
+
+import io.github.sifu.springbootsell.pojo.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * 商品DAO
+ *
+ * @author sifu
+ */
+public interface ProductInfoRepository extends JpaRepository<ProductInfo,Integer> {
+
+    /**
+     * 列出所有相同状态的商品信息
+     *
+     * @param productStatus
+     * @return
+     */
+    List<ProductInfo> findByProductStatus(Integer productStatus);
+
+}
