fc59715c9b9eb32560a5dc2d05101143e34e03d8
Gitsifu <2587107273.@qq.com>
2017-11-02 10:28:36 +0800

    更新

---
+++ src/main/java/io/github/sifu/springbootsell/controller/BuyerProductController.java
@@ -0,0 +1,82 @@
+package io.github.sifu.springbootsell.controller;
+
+import io.github.sifu.springbootsell.pojo.ProductCategory;
+import io.github.sifu.springbootsell.pojo.ProductInfo;
+import io.github.sifu.springbootsell.service.CategoryService;
+import io.github.sifu.springbootsell.service.ProductService;
+import io.github.sifu.springbootsell.vo.ProductInfoVO;
+import io.github.sifu.springbootsell.vo.ProductVO;
+import io.github.sifu.springbootsell.vo.ResultVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 买家商品
+ *
+ * @author sifu
+ */
+@RestController
+@RequestMapping("/buyer/product")
+public class BuyerProductController {
+
+    private final ProductService productService;
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public BuyerProductController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/list")
+    public ResultVO list() {
+        //1.查询多有的上架商品
+        List<ProductInfo> productInfoList = productService.findUpAll();
+        //2.查询类目（一次性查询）
+//        List<Integer> categoryTypeList = new ArrayList<>();
+        //传统方法
+        List<Integer> categoryTypeList = productInfoList.stream()
+                .map(e->e.getCategoryType())
+                .collect(Collectors.toList());
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
+        categoryService.findByCategoryTypeIn(categoryTypeList);
+        //3.数据拼装
+        List<ProductVO> productVOList = new ArrayList<>();
+        for (ProductCategory productCategory : productCategoryList){
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for (ProductInfo productInfo : productInfoList){
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())){
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo,productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
+
+        ResultVO resultVO = new ResultVO();
+
+        resultVO.setData(productVOList);
+        resultVO.setCode(0);
+        resultVO.setMsg("成功");
+
+        return resultVO;
+    }
+
+
+
+}
